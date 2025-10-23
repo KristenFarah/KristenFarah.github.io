4d3bbbd17b79bd2bb65409766723741bc487b8f3
KristenFarah <kristen.farah28@gmail.com>
2025-10-23 11:04:22 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Back to top feature 
+const backToTopButton = document.getElementById("backToTop");
+
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
